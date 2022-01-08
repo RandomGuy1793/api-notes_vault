977c4b3038545675751b8be70d8d69a98a05772a
Faisal Alam <faisal.alam.1999@gmail.com>
2022-01-08 11:48:35 +0530

    added PORT via env variable and removed logfile

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express=require('express')
-const config=require('config')
 const app=express()
 
 require('./startup/errorLog')()
@@ -8,5 +7,5 @@ require('./startup/routes')(app)
 require('./startup/db')()
 require('./startup/prod')(app)
 
-const PORT=config.get('PORT')
+const PORT=process.env.PORT || 5000
 app.listen(PORT, ()=>console.log(`Server active on port ${PORT}`))
--- startup/config.js
+++ startup/config.js
@@ -4,7 +4,4 @@ module.exports=function(){
     if(!config.get('jwtKey')){
         throw new Error('notes_vault_jwtKey is not set as environment variable')
     }
-    if(!config.get('PORT')){
-        throw new Error('notes_vault_port is not set as environment variable')
-    }
 }

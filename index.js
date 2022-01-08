f86551283e11adde1073508208134d1e55601a88
Faisal Alam <faisal.alam.1999@gmail.com>
2022-01-08 11:01:36 +0530

    added helmet and compression packages and added mongoDB connStr in env variable

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ require('./startup/errorLog')()
 require('./startup/config')()
 require('./startup/routes')(app)
 require('./startup/db')()
+require('./startup/prod')(app)
 
 const PORT=config.get('PORT')
 app.listen(PORT, ()=>console.log(`Server active on port ${PORT}`))
